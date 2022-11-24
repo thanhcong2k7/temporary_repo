de02d5bb3cb1ff7832c9ae3479a1b41e5141e10f
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2022-11-24 22:25:03 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,6 @@
-curl -Lo ngrok.tgz "https://is.gd/5eH56Y"
-tar -xvf ngrok.tgz
-rm -f ngrok.tgz
-read -p "Paste authtoken here: " atkzz
-./ngrok config add-authtoken $atkzz
 echo Installing QEMU...
 sudo apt -q install -y qemu-system-x86
-echo Preparing...
-curl -Lo 7X86SLKM.iso "https://docs.google.com/uc?export=download&confirm=t&id=1mj9pW_oR9slCgm77LPOI5mPQZyNbDsKY"
-qemu-img create w7.img -f raw 4G
+echo Starting...
 qemu-system-x86_64 -cdrom 7X86SLKM.iso -m 4096M -drive format=raw,file=w7.img -net nic -net user -vnc :5 &
 echo System is up!
 echo Now you just need to copy the URL at line "Forwarding ..."
